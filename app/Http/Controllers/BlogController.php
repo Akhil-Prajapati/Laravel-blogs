7e4b17c918be4f8ec98900163460bdf3c30b11d7
AkhilNinja <akhiljprajapati99@gmail.com>
2021-03-11 18:43:24 +0530

    init

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Blog;
+
+class BlogController extends Controller
+{
+
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $blogs = auth()->user()->blogs;
+        return view('blogs.index',['blogs' => $blogs]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('blogs.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'body' => 'required|string'
+        ]);
+        Blog::create([
+            "title" => $request->get('title'),
+            "body" => $request->get('body'),
+            "user_id" => auth()->user()->id
+        ]);
+
+        return redirect()->route('blogs.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $blog = Blog::find($id);
+        return view('blogs.edit',['blog' => $blog]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'body' => 'required|string'
+        ]);
+        $blog = Blog::find($id);
+        $blog->update([
+            "title" => $request->get('title'),
+            "body" => $request->get('body'),
+            "user_id" => auth()->user()->id
+        ]);
+
+        return redirect()->route('blogs.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Blog::find($id)->delete();
+        return redirect()->route('blogs.index');
+    }
+}
---
+++ resources/views/blogs/index.blade.php
@@ -0,0 +1,89 @@
+<x-app-layout class="dark:bg-gray-800">
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:bg-gray-800 dark:text-white leading-tight">
+            {{ __('Blogs') }}
+        </h2>
+    </x-slot>
+
+    <div class="container my-12 mx-auto px-4 md:px-12 dark:bg-gray-800">
+        <a href="{{ route('blogs.create') }}" class="inline-flex items-center h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+            <span>Create Blog</span>
+            <svg class="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+        </a>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4 dark:text-white">
+            <!-- Column -->
+            @if(count($blogs) > 0)
+                @foreach ($blogs as $blog)
+                    <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+                        <!-- Article -->
+                        <article class="overflow-hidden rounded-lg shadow-lg">
+
+                            <a href="#">
+                                <img alt="Placeholder" class="block h-auto w-full" src="https://picsum.photos/600/400/?random">
+                            </a>
+
+                            <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                                <h1 class="text-lg">
+                                    <a class="no-underline hover:underline text-black" href="#">
+                                        {{ $blog->title }}
+                                    </a>
+                                </h1>
+                                <p class="text-grey-darker text-sm">
+                                    {{ $blog->created_at }}
+                                </p>
+                            </header>
+                            <main class="lex items-center justify-between leading-tight p-2 md:p-4">
+                                <p>
+                                    {{ $blog->body }}
+                                </p>
+                            </main>
+                            <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                                <a class="flex items-center no-underline hover:underline text-black" href="#">
+                                    <img alt="Placeholder" class="block rounded-full" src="https://picsum.photos/32/32/?random">
+                                    <p class="ml-2 text-sm">
+                                        {{ $blog->user->name }}
+                                    </p>
+                                </a>
+                                <form action="{{ route('blogs.destroy' ,$blog->id) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="no-underline text-grey-darker hover:text-red-dark text-red-600">
+                                        <span>Delete</span>
+                                        <i class="fa fa-heart"></i>
+                                    </button>
+                                </form>
+                                <a class="no-underline text-grey-darker hover:text-red-dark flote-left" href="{{ route('blogs.edit',  $blog->id ) }}">
+                                    <span>Edit</span>
+                                    <i class="fa fa-heart"></i>
+                                </a>
+                            </footer>
+
+                        </article>
+                        <!-- END Article -->
+                    </div>
+                @endforeach
+            @else
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+                <!-- Article -->
+                <article class="overflow-hidden rounded-lg">
+                    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-indigo-500">
+                        <span class="text-xl inline-block mr-5 align-middle">
+                        <i class="fas fa-bell" />
+                        </span>
+                        <span class="inline-block align-middle mr-8">
+                        <b class="capitalize">Blogs!   </b>   There is nothing near you
+                        </span>
+                        <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
+                        <span>×</span>
+                        </button>
+                    </div>
+                </article>
+            </div>    
+            @endif
+            <!-- END Column -->
+
+        </div>
+    </div>
+</x-app-layout>
