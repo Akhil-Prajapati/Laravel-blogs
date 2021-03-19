85208d407e7dfa3cb7c37b612fed9fff327c197d
Akhil Prajapati <akhiljprajapati99@gmail.com>
2021-03-19 15:00:08 +0530

    add delete in home page of blog

--- resources/views/blogs/blog.blade.php
+++ resources/views/blogs/blog.blade.php
@@ -6,10 +6,10 @@
         </h2>
     </x-slot>
     
-    <div class="container my-12 mx-auto px-4 md:px-12 ">
+    <div class="container my-12 mx-auto px-4 md:px-12">
         <!-- component -->
         <!-- This is an example component -->
-        <div class="bg-white overflow-auto shadow-none border-b-4 border-r-4 border-gray-400  border-opacity-25" >
+        <div class="bg-white overflow-auto shadow-none border-b-4 border-r-4 border-gray-400  border-opacity-25 h-auto" >
             <div class="grid grid-cols-3 min-w-full">
                 
                 <div class="col-span-2 w-full">
@@ -28,7 +28,7 @@
                     </header>
                     <div class="overflow-y-auto md:h-28 sm:h-10 lg:h-60">
                     @foreach ($comments as $comment)
-                        <div class="pt-1">
+                        <div class="pt-1 pr-5">
                             <div class="text-sm mb-2 flex flex-start items-center">
                                 <div v-if="showUserImage">
                                     <a href="#" class="cursor-pointer flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
@@ -43,12 +43,29 @@
                                 @else
                                     <a class="cursor-pointer">{{ 'Unknown' }}:</a>   
                                 @endif
-                                    <span class="text-gray-700 font-medium ml-1">
+                                    <span class="text-justify break-all text-gray-700 font-medium ml-1">
                                         {{ $comment->text }}
                                     </span>
                                 </p>
+                                <div class="pl-24 object-right">
+                                @if (!Auth::guest())
+                                @if ($comment->user_id == Auth::user()->id || $blog->user_id == Auth::user()->id )
+                                <form action="{{ route('blogs.comments.destroy', [$blog->id, $comment->id]) }}" method="post">
+                                    <button class="focus:outline-none  relative inline-flex items-center bg-white rounded-full border border-gray-300 px-3 py-0.5 text-sm">
+                                        <div class="absolute flex-shrink-0 flex items-center justify-center">
+                                            <span class="h-1.5 w-1.5 rounded-full bg-red-500" aria-hidden="true"></span>
+                                        </div>
+                                            @csrf 
+                                            @method('delete')
+                                            <span class="ml-3.5 font-medium text-gray-900"> 🗑 </span>
+                                    </button>   
+                                </form>
+                                @endif
+                                @endif
                             </div>
-                        </div> 
+                            </div>
+                            
+                        </div>
                     @endforeach
                     </div>
                     <div class="absolute bottom-0 left-0 right-0 pl-4">
@@ -59,6 +76,11 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                                     </svg>
                                 </span>
+                                <span class=" mr-3 inline-flex items-center cursor-pointer">
+                                    <svg class="fill-heart text-gray-700 inline-block h-7 w-7 heart" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                    </svg>
+                                </span>
                             </div>
                             <a href="{{ url('read/' . $blog->id . '?comments=' . ($limit+5)) }}">
                                 <span class="text-gray-600 text-sm font-bold">
@@ -66,7 +88,7 @@
                                 </span>
                             </a>
                             <a href="{{ route('blogs.comments.index',$blog->id) }}">
-                                <span class="text-gray-600 text-sm font-bold float-right">
+                                <span class="text-gray-600 text-md pr-5 font-bold float-right">
                                     view all
                                 </span>
                             </a>
@@ -90,7 +112,7 @@
         </div>
         
         <div class="col-span-2 w-full border-2 border-gray-400  border-opacity-25 mt-5 mb-5">
-            <div class="capitalize text-2xl font-normal pl-2 leading-normal mt-0 mb-2    ">
+            <div class="capitalize text-3xl font-normal pl-2 leading-normal mt-0 mb-2  old  ">
                 {{ $blog->title }}
             </div>
             <div class=" text-xl font-normal leading-normal mt-0 mb-2 text-gray-800 text-center whitespace-normal">
--- resources/views/blogs/home.blade.php
+++ resources/views/blogs/home.blade.php
@@ -14,13 +14,13 @@
                     </div>
                     <div class="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                         <div class="mb-8">
-                            <p class="text-sm text-grey-dark flex items-center">
+                            {{-- <p class="text-sm text-grey-dark flex items-center">
                                 <svg class="text-grey w-3 h-3 mr-2" xmlns="{{ $blog->url }}" viewBox="0 0 20 20">
                                     <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
                                 </svg>
                                 Members only
-                            </p>
-                            <div class="text-black font-bold text-xl mb-2">
+                            </p> --}}
+                            <div class=" capitalize text-black font-bold text-xl mb-2">
                                 <a href="{{ url('/read/' . $blog->id) }}">
                                     {{ $blog->title }}
                                 </a>
